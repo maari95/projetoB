ac767cc197fa6001108042ec57871a98da38c5ce
Técnico em Informática - 2022.3 <wellingtonalexandre857@gmail.com>
2023-10-04 16:15:51 -0300

    asfag

--- src/app/page/cadastro/cadastro.component.ts
+++ src/app/page/cadastro/cadastro.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CadastroService } from 'src/app/model/cadastro.service';
+import { Router } from '@angular/router';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,5 +10,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent {
+  // nome:string='';
+  email: string = '';
+  senha: string = '';
+  mensagem: string = '';
+  // bio: string='';
 
+  constructor(private service: CadastroService, private router: Router, /* private ActionSheetCtrl: ActionSheetController,  */private firedata: AngularFireDatabase) { }
+
+  ngOnInit() {
+  }
+
+
+  async registrar() {
+    if (this.email && this.senha) {
+      try {
+        const result = await this.service.cadastrar(this.email, this.senha);
+
+        console.log('Usuário Cadastrado');
+        this.router.navigate(['/login']);
+      } catch (error) {
+        console.error('Erro ao cadastrar usuário', error);
+      }
+    } else {
+      console.log('error');
+    }
+  }
+
+  // salvarNomeUsuario(){
+  //   if(this.nome.trim() !== ''){
+  //     localStorage.setItem('user', this.nome);
+  //   }
+  // }
 }
